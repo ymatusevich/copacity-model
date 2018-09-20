da9c5c7085f600641d7e6a80478f48bb1b6ee5b8
Yury Matusevich <yur4ik93@mail.ru>
2018-09-21 00:25:08 +0300

    resource page improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
 
     block_given? ? link_to(options, html_options) { yield } : link_to(name, options, html_options)
   end
+
+  def status_class(project)
+    Settings.project.classes.to_h[project.status_name]
+  end
+
+  def resource_type_name(id)
+    Settings.resource_types.to_h.key(id)
+  end
+
+  def currency_name(currency)
+    Settings.currencies.to_h.key(currency)
+  end
 end
